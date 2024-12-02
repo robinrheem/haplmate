03e96cc7e68ad8cafd47a029d0f792e2966194fb
Robin Rheem <robinrheem@gmail.com>
2024-12-02 10:56:47 -0800

    feat: input required args and print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+/// Estimating haplotypes with Simulated Annealing and Expectation-Maximization
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Args {
+    /// Input FASTA file(s)
+    #[arg(value_name = "FILE", default_value = "-")]
+    files: Vec<String>,
+    /// Number of samples
+    #[arg(short, long)]
+    samples: usize,
+    /// Number of reads in the alignment file
+    #[arg(short, long)]
+    reads: usize,
+    /// Length of aligned reads
+    #[arg(short('l'), long)]
+    sequence_length: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    dbg!(args);
 }

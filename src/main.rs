9959c7f5906db57164615ee44b1e2fced71a6175
Robin Rheem <robinrheem@gmail.com>
2025-01-05 17:22:56 -0800

    feat: implement options and remove unncessary required

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,46 @@ struct Args {
     /// Input FASTA file(s)
     #[arg(value_name = "FILE", default_value = "-")]
     files: Vec<String>,
-    /// Number of samples
-    #[arg(short, long)]
-    samples: usize,
-    /// Number of reads in the alignment file
-    #[arg(short, long)]
-    reads: usize,
-    /// Length of aligned reads
-    #[arg(short('l'), long)]
-    sequence_length: usize,
+    /// Maximum allowed mismatch between haplotypes and reads
+    #[arg(short = 'm', long, default_value = "15")]
+    mismatches: usize,
+    /// Maximum number of EM iterations during intermediate steps
+    #[arg(short, long, default_value = "20")]
+    em_iterations: usize,
+    /// Strength of annealing
+    #[arg(long, default_value = "0.1")]
+    sa_schedule: f64,
+    /// Lambda1 value (for testing purposes only)
+    #[arg(long, default_value = "0.1")]
+    lambda1: f64,
+    /// Lambda2 value (for testing purposes only)
+    #[arg(long, default_value = "0.1")]
+    lambda2: f64,
+    /// Sequencing error
+    #[arg(short = 'd', long = "seqerr", default_value = "0.04")]
+    error_rate: f64,
+    /// Minimum temp to reach in simulated annealing
+    #[arg(long, default_value = "0.0")]
+    sa_min_temperature: f64,
+    /// Starting maximum temp in simulated annealing
+    #[arg(long, default_value = "10.0")]
+    sa_max_temperature: f64,
+    /// Number of iterations in simulated annealing
+    #[arg(long, default_value = "500")]
+    sa_iterations: usize,
+    /// Interval between calling EM
+    #[arg(long, default_value = "10")]
+    em_interval: usize,
+    /// Number of reruns of optimization algorithm
+    #[arg(long, default_value = "5")]
+    sa_reruns: usize,
+    /// Delta to determine intermediate EM convergence steps
+    #[arg(long, default_value = "0.5")]
+    em_cdelta: f64,
 }
 
+/// TODO: Automatically read and get numbers of:
+/// reads, samples(number of files), length of aligned reads(sequence length)
 fn main() {
     let args = Args::parse();
     dbg!(args);
